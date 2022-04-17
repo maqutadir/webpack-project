e4a8ab4620278f0a2f59b4083f93ad268f20a709
Syed Maqutadir Rehman <maqutadir.rehman@gmail.com>
2022-04-17 19:32:06 -0400

    Babel transpiling in action

--- src/index.js
+++ src/index.js
@@ -3,3 +3,22 @@ import { subjects } from "./subjects"
 (function consoleLog() {
     console.log(`Here are all the subjects ${subjects} served by webpack project`)
 })()
+
+const eachSubject = subjects.forEach((subject) => {
+    console.log('This subject is', subject)
+})
+
+let players = {player1: 'VK', player2: 'SRT', player3: 'MSD'}
+let playerCount = 3
+
+const addPlayer = (playerName) => {
+    playerCount += 1
+    //players[`players${playerCount}`] = playerName
+    players = {...players, [`player${playerCount}`]: playerName}
+}
+
+addPlayer('MS')
+addPlayer('RS')
+addPlayer('JB')
+addPlayer('BK')
+console.log('Players are', players)
